8ee3473b14c9e61cf39b63da2d026d6128826d30
rushikadam12 <118955058+rushikadam12@users.noreply.github.com>
2024-01-04 11:07:54 +0530

    push3

--- app/login/Register.js
+++ app/login/Register.js
@@ -1,8 +1,10 @@
-const mongoose = require("mongoose");
 require('../DB/db')
+const mongoose = require("mongoose");
 const User = require('../DB/register')
 const express = require("express");
 const router = express.Router();
+const Hpassword=require('../login/HashPass')
+
 router.post("/", async (req, res) => {
   const { username, email, password, budget } = req.body;
   console.log(req.body);
@@ -12,23 +14,21 @@ router.post("/", async (req, res) => {
       return res.json({ error: "pls enter your all fields" });
     }
 
-    const existingUser = await User.findOne({ email: email })
+    const existingUser = await User.findOne({ email: email })//search for email in database
     if (existingUser) {
       return res.status(422).json({ error: "email already present" });
     }
-    const user = new User({ username, email, password, budget })
+
+    const hpass=await Hpassword(password);//function for convert into hash format
+    const user =new User({ username, email, password:hpass, budget })//add the data
+
     const resp = await user.save();
-    res.send({ message: "Data added successfully", resp: resp });
+    res.status(200).send({ message: "Data added successfully", resp: resp });
     console.log("data added");
-    //.then(()=>{
-    //     res.status(200).json({message:'data added successfully'})
-    // }).catch((err)=>{
-    //     console.log(error)
-    //     res.status(500).json({error:err})
-    // })
+  
   } catch (error) {
     console.log(error);
-    res.send({ error: error });
+    res.status(500).send({ error: error });
   }
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -5,34 +5,33 @@ const exp=require('./app/DB/exp.js')
 const bodyParser=require('body-parser')
 const cors=require('cors')
 const Register=require('./app/login/Register.js')
+const Login=require('./app/login/Login.js')
+const session=require('express-session')
 app=express();
 app.use(express.json())
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:5171'],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+}));
+app.use(session({
+    key:"userId",
+    secret:process.env.SECURE_KEY,
+    resave:true,
+    saveUninitialized:false,
+    cookie:{
+        expires:60*60*24,
+        
+    },
+}))
 
 
-// app.get('/',async(req,res)=>{
-//     const currentDate = new Date();
-//     const formattedDate = currentDate.toLocaleDateString('en-US');
-//     const currentTime = currentDate.toLocaleTimeString('en-US', { hour12: false });
-//     const food=new exp({
-//         ExpenseType:"buy milk",
-//         Date:formattedDate,
-//         Time:currentTime,
-//         cost:40
-//     })
-//     try{
-//         await food.save();
-//         res.send('Data added successfully')
-//         console.log("data added")
-//     }catch(error){
-//         console.log(error)
-//     }
-
-// })
 app.use("/api/Register",Register)
-connectDb().then(()=>{
+app.use("/api/Login",Login)
 
+
+connectDb().then(()=>{
 app.listen(process.env.PORT||3000,()=>{
     console.log('server is online')
 })
