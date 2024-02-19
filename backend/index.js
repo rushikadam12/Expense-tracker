7db5eba3f588bdfc7269c0247fc5a8ba20d1313d
rushikadam12 <118955058+rushikadam12@users.noreply.github.com>
2024-02-19 12:04:09 +0530

    changes in config add the proxy

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,51 @@
-require('dotenv').config();
-const express=require('express');
-const connectDb=require('./app/DB/db.js')
-const bodyParser=require('body-parser')
-const cors=require('cors')
-const Register=require('./app/login/Register.js')
-const Login=require('./app/login/Login.js')
-const session=require('express-session')
-const addExpense=require('./app/Expense/addExpense.js')
-const ByDate=require('./app/Sort/ByDate.js')
-const auth=require('./app/authentication/authentication.js')
-const UserData=require('./app/login/UserData.js')
-const verifyToken=require('./app/middleware/VerifyToken.js')
-const deleteExpense=require('./app/Expense/deleteExpense.js')
-const rateLimit=require('express-rate-limit')
-const cookieParser=require('cookie-parser');
-const Logout=require('./app/login/Logout.js')
+require("dotenv").config();
+const express = require("express");
+const connectDb = require("./app/DB/db.js");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Register = require("./app/login/Register.js");
+const Login = require("./app/login/Login.js");
+const session = require("express-session");
+const addExpense = require("./app/Expense/addExpense.js");
+const ByDate = require("./app/Sort/ByDate.js");
+const auth = require("./app/authentication/authentication.js");
+const UserData = require("./app/login/UserData.js");
+const verifyToken = require("./app/middleware/VerifyToken.js");
+const deleteExpense = require("./app/Expense/deleteExpense.js");
+const rateLimit = require("express-rate-limit");
+const cookieParser = require("cookie-parser");
+const Logout = require("./app/login/Logout.js");
 
-app=express();
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors({
-    origin:"https://expense-tracker-rose-xi.vercel.app",
-    credentials:true,
+app = express();
+app.set('trust proxy', 1);
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://expense-tracker-rose-xi.vercel.app",
+    credentials: true,
+  })
+);
 
-}));
+const limiter = rateLimit({
+  //to limit the api request from user
+  windowMs: 60 * 1000, // 1 minute
+  max: 50, // max requests per minute
+});
+app.use(limiter);
 
+app.use("/api/Register", Register);
+app.use("/api/Auth", auth);
+app.use("/api/Login", Login);
+app.use("/api/logout", verifyToken, Logout);
+app.use("/api/AddExpense", verifyToken, addExpense);
+app.use("/api/Expenses", verifyToken, ByDate);
+app.use("/api", verifyToken, UserData);
+app.use("/api/Delete", verifyToken, deleteExpense);
 
-const limiter = rateLimit({//to limit the api request from user
-    windowMs: 60 * 1000, // 1 minute
-    max: 50, // max requests per minute
+connectDb().then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("server is online");
   });
-app.use(limiter)
-
-app.use("/api/Register",Register)
-app.use("/api/Auth",auth)
-app.use("/api/Login",Login)
-app.use("/api/logout",verifyToken,Logout)
-app.use("/api/AddExpense",verifyToken,addExpense)
-app.use("/api/Expenses",verifyToken,ByDate)
-app.use("/api",verifyToken,UserData)
-app.use("/api/Delete",verifyToken,deleteExpense)
-
-
-connectDb().then(()=>{
-app.listen(process.env.PORT||3000,()=>{
-    console.log('server is online')
-})
-
-})
+});
