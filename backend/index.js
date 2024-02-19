ca9c07527b61ab31dc2d6451827d6676eca99330
rushikadam12 <118955058+rushikadam12@users.noreply.github.com>
2024-02-19 11:09:21 +0530

    change the cors

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://pennywise-aypl.onrender.com/"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 
